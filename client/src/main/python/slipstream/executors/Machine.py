208a7b283d90e22d6490fc0648446c6a721b3d0d
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2014-09-15 01:19:53 +0200

    Publish abort message (as formatted exception) on failures to create machine executor.

--- client/src/main/python/slipstream/executors/Machine.py
+++ client/src/main/python/slipstream/executors/Machine.py
@@ -1,7 +1,7 @@
 """
  SlipStream Client
  =====
- Copyright (C) 2013 SixSq Sarl (sixsq.com)
+ Copyright (C) 2014 SixSq Sarl (sixsq.com)
  =====
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -16,7 +16,14 @@
  limitations under the License.
 """
 
+import sys
+import os
+import traceback
+from slipstream import util
+
 from slipstream.ConfigHolder import ConfigHolder
+from slipstream.SlipStreamHttpClient import SlipStreamHttpClient
+from slipstream.NodeDecorator import NodeDecorator
 
 
 class Machine(object):
@@ -26,5 +33,53 @@ class Machine(object):
         configHolder.assign(self)
 
     def execute(self):
-        executor = self.executorFactory.createExecutor(self.configHolder)
+        try:
+            executor = self.executorFactory.createExecutor(self.configHolder)
+        except Exception as ex:
+            self._publish_abort_and_fail("Machine executor creation failed", ex)
         executor.execute()
+
+    def _publish_abort_and_fail(self, message, exception):
+        util.printError('Failing... %s: %s' % (message, str(exception)))
+        traceback.print_exc()
+        AbortExceptionPublisher(self.configHolder).publish(message, sys.exc_info())
+        raise exception
+
+
+class AbortExceptionPublisher(object):
+
+    @staticmethod
+    def _get_verbosity_level(config_holder):
+        try:
+            return int(config_holder.verboseLevel)
+        except:
+            return int(os.environ.get('SLIPSTREAM_VERBOSITY_LEVEL', '0'))
+
+    @staticmethod
+    def _format_exception_message(message, exc_info, verbosity_level=0):
+        "exc_info - three-element list as returned by sys.exc_info()"
+        if verbosity_level > 1:
+            msg_list = traceback.format_exception(*exc_info)
+        else:
+            msg_list = traceback.format_exception_only(*exc_info[:2])
+        return '%s: %s' % (message, ''.join(msg_list).strip())
+
+    def __init__(self, config_holder):
+        """
+        config_holder: ConfigHolder object
+        """
+        self.ss_client = SlipStreamHttpClient(config_holder)
+        self.verbosity_level = self._get_verbosity_level(config_holder)
+
+    def publish(self, message, exc_info):
+        """Publish formatted exception as abort message on the Run.
+        message: string
+        exc_info: three-element list as returned by sys.exc_info()
+        """
+        msg = self._format_exception_message(message, exc_info, self.verbosity_level)
+        self._publish_abort(msg)
+
+    def _publish_abort(self, message):
+        self.ss_client.ignoreAbort = True
+        abort = NodeDecorator.globalNamespacePrefix + NodeDecorator.ABORT_KEY
+        self.ss_client.setRuntimeParameter(abort, message)
