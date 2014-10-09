b29149aba2939778f376588b5e82c97d04b318e1
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2014-10-09 16:43:25 +0200

    Return an empty list of networks if none are defined in the parameter.

--- client/src/main/python/slipstream/NodeInstance.py
+++ client/src/main/python/slipstream/NodeInstance.py
@@ -116,7 +116,13 @@ class NodeInstance(object):
         return self.__get('network', default_value)
 
     def get_networks(self):
-        return self.get_cloud_parameter('networks', '').split(',')
+        '''Return list of user provided network names, or an empty list instead.
+        '''
+        networks = self.get_cloud_parameter('networks', '')
+        if networks:
+            return [n.strip() for n in networks.split(',')]
+        else:
+            return []
 
     def get_platform(self):
         return self.__get(NodeDecorator.IMAGE_PLATFORM_KEY, 'linux')
