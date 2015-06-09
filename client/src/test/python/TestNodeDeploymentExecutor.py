slipstream/SlipStreamClient

commit 2cdd3ecef8009784fc7482a01f6f4ced2542a5ff

The check for the scale state consistensy is now made only on orchestrators to prevent n**2 requests to the SS server. with n=<number of VMs in the Run>. Now the number of requests needed for this check is n*c with c=<number of Clouds involved in the Run>.

client/src/main/python/slipstream/executors/node/NodeDeploymentExecutor.py
@@ -125,7 +125,7 @@ class NodeDeploymentExecutor(MachineExecutor):
         return self.SCALE_ACTION_TO_TARGET.get(action, None)
 
     def _execute_scale_action_target(self):
-        scale_action = self.wrapper.get_global_scale_action()
+        scale_action = self.wrapper.get_scale_action()
         if scale_action:
             # TODO: Add local scale actions (ondiskresize, etc)
             target = self._get_target_on_scale_action(scale_action)

client/src/main/python/slipstream/executors/orchestrator/OrchestratorDeploymentExecutor.py
@@ -49,6 +49,7 @@ class OrchestratorDeploymentExecutor(MachineExecutor):
     def onExecuting(self):
         super(OrchestratorDeploymentExecutor, self).onExecuting()
         self._complete_state_for_failed_node_instances()
+        self.wrapper.check_scale_state_consistency()
 
     @override
     def onSendingReports(self):

client/src/main/python/slipstream/wrappers/BaseWrapper.py
@@ -265,6 +265,19 @@ class BaseWrapper(object):
         state = self._get_global_scale_state()
         return self._state_to_action(state)
 
+    def get_scale_action(self):
+        state = self.get_scale_state()
+        return self._state_to_action(state)
+
+    def check_scale_state_consistency(self):
+        states_node_instances = self._get_effective_scale_states()
+        states_node_instances.pop(self.SCALE_STATE_REMOVED, None)
+
+        if len(states_node_instances) > 1:
+            msg = "Inconsistent scaling situation. Single scaling action allowed," \
+                  " found: %s" % states_node_instances
+            raise Exceptions.ExecutionException(msg)
+
     def get_scaling_node_and_instance_names(self):
         '''Return name of the node and the corresponding instances that are
         currently being scaled.

commit 8253b93dc3eb1a1c68e4a2c48bd9dbdfdca658b5

Fixing a bug introduced in 2cdd3ec where the node instance scale state is requested instead of the global one.

client/src/main/python/slipstream/executors/node/NodeDeploymentExecutor.py
@@ -145,7 +145,7 @@ class NodeDeploymentExecutor(MachineExecutor):
         return self.SCALE_ACTION_TO_TARGET.get(action, None)
 
     def _execute_scale_action_target(self):
-        scale_action = self.wrapper.get_scale_action()
+        scale_action = self.wrapper.get_global_scale_action()
         if scale_action:
             # TODO: Add local scale actions (ondiskresize, etc)
             target = self._get_target_on_scale_action(scale_action)

client/src/test/python/TestNodeDeploymentExecutor.py
@@ -62,3 +62,12 @@ class TestNodeDeploymentExecutor(TestCloudConnectorsBase):
                           *(target, {}, True))
         assert 1 == nde.wrapper.fail.call_count
 
+    def test_execute_scale_action_target_gets_global_scale_action(self):
+        wrapper = Mock()
+        wrapper.get_scale_action = Mock(return_value=None)
+        wrapper.get_global_scale_action = Mock(return_value=None)
+        nde = NodeDeploymentExecutor(wrapper, configHolder=self.ch)
+        nde._execute_scale_action_target()
+        assert True == nde.wrapper.get_global_scale_action.called
+        assert False == nde.wrapper.get_scale_action.called
+