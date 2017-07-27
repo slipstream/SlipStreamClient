908f3c66c249589d1d9d103d1dbcc6b06d327646
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-07-27 08:52:32 +0200

    try with laxed check of the existence of the cookie - match only domain.

--- client/src/main/python/slipstream/HttpClient.py
+++ client/src/main/python/slipstream/HttpClient.py
@@ -112,7 +112,7 @@ class HttpClient(object):
         self.cookie = cookie
         self.username = username
         self.password = password
-        self.host_cert_validate = False
+        self.host_cert_verify = False
         self.verboseLevel = util.VERBOSE_LEVEL_NORMAL
         self.cookie_filename = util.DEFAULT_COOKIE_FILE
 
@@ -210,7 +210,8 @@ class HttpClient(object):
             try:
                 auth = ()
                 cookie_used = False
-                if self.session_cookie_exists(self.session, url):
+                if self.session_cookie_exists(self.session, url,
+                                              no_path_check=True):
                     cookie_used = True
                 elif self.username and self.password:
                     self._log_debug("No session cookie found. Using Basic "
@@ -224,7 +225,7 @@ class HttpClient(object):
                                             auth=auth,
                                             data=body,
                                             headers=headers,
-                                            verify=self.host_cert_validate)
+                                            verify=self.host_cert_verify)
                 if resp.status_code == 401 and cookie_used and (
                             self.username and self.password):
                     resp = self.session.request(method, url,
@@ -233,7 +234,7 @@ class HttpClient(object):
                                                     self.password),
                                                 data=body,
                                                 headers=headers,
-                                                verify=self.host_cert_validate)
+                                                verify=self.host_cert_verify)
                 return resp
             except requests.exceptions.InvalidSchema as ex:
                 raise Exceptions.ClientError("Malformed URL: %s" % ex)
@@ -331,17 +332,21 @@ class HttpClient(object):
             self._set_oldstyle_cookie_on_session(url)
 
     @staticmethod
-    def session_cookie_exists(session, url, cookie_name=DEFAULT_SS_COOKIE_NAME):
+    def session_cookie_exists(session, url, no_path_check=False,
+                              cookie_name=DEFAULT_SS_COOKIE_NAME):
         _url = urlparse(url)
         jar = RequestsCookieJar()
         jar.update(session.cookies)
-        url_path = _url.path.split('/')
-        for n in range(len(url_path), 0, -1):
-            path = '/'.join(url_path[0:n]) or '/'
-            cookies = jar.get_dict(domain=_url.netloc, path=path)
-            if cookie_name in cookies:
-                return True
-        return False
+        if no_path_check:
+            return len(jar.get_dict(domain=_url.netloc)) > 0
+        else:
+            url_path = _url.path.split('/')
+            for n in range(len(url_path), 0, -1):
+                path = '/'.join(url_path[0:n]) or '/'
+                cookies = jar.get_dict(domain=_url.netloc, path=path)
+                if cookie_name in cookies:
+                    return True
+            return False
 
     def _log_normal(self, message):
         util.printDetail(message, self.verboseLevel,
